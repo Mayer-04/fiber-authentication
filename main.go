59d288f70d4b122c38f3072edd566db2409c4537
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-28 13:29:56 -0500

    Conexión a base de datos

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/Mayer-04/fiber-authentication/config"
+	"github.com/Mayer-04/fiber-authentication/pkg/database"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+
+	port := config.LoadEnvVariables().Port
+
+	database.ConnectDatabase()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	addr := fmt.Sprintf(":%d", port)
+
+	app.Listen(addr)
+
 }
