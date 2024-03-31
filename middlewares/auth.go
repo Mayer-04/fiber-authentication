d2a72de0608ee302291a8d35c9c32961d4d3802a
Mayer Chaves <andres.20.chaves@gmail.com>
2024-03-31 14:02:25 -0500

    añadiendo verificación del token 👌

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,1 +1,52 @@
 package middlewares
+
+import (
+	"log"
+
+	"github.com/Mayer-04/fiber-authentication/handler"
+	"github.com/gofiber/fiber/v2"
+)
+
+func VerifyToken(c *fiber.Ctx) error {
+	const unauthorizedMsg = "Unauthorized, no token provided"
+	const invalidTokenMsg = "Invalid Token"
+
+	cookie := c.Cookies("Authorization")
+
+	if cookie == "" {
+		return sendUnauthorized(c, unauthorizedMsg)
+	}
+
+	err := handler.VerifyToken(cookie)
+
+	if err != nil {
+		log.Printf("failed to generate token: %v", err)
+		return sendUnauthorized(c, invalidTokenMsg)
+	}
+
+	return c.Next()
+}
+
+func sendUnauthorized(ctx *fiber.Ctx, msg string) error {
+	return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+		"success": false,
+		"message": msg,
+	})
+}
+
+// func VerifyToken(c *fiber.Ctx) error {
+// 	cookie := c.Cookies("Authorization")
+
+// 	if cookie == "" {
+// 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"success": false, "message": "Unauthorized, no token provided"})
+// 	}
+
+// 	err := handler.VerifyToken(cookie)
+
+// 	if err != nil {
+// 		log.Printf("failed to generate token: %v", err)
+// 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"success": false, "message": "Invalid Token"})
+// 	}
+
+// 	return c.Next()
+// }
