7893434aee2ce201fa7b144ea3c7df745354cb47
Mayer Chaves <andres.20.chaves@gmail.com>
2024-03-31 12:41:48 -0500

    cambios en los modelos

--- models/user.go
+++ models/user.go
@@ -8,7 +8,6 @@ type User struct {
 	gorm.Model
 
 	Name     string `gorm:"type:varchar(100);not null" json:"name"`
-	UserName string `gorm:"type:varchar(100);not null" json:"username"`
 	Password string `gorm:"type:varchar(100);not null" json:"password"`
 	Email    string `gorm:"uniqueIndex;type:varchar(100);not null" json:"email"`
 }
