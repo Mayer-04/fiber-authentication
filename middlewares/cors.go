0068b8deaa38bf0836ff26d621e69a5489700398
Mayer Chaves <andres.20.chaves@gmail.com>
2024-03-31 12:43:11 -0500

    cambiando implementación

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,12 +1,23 @@
 package middlewares
 
-import "github.com/gofiber/fiber/v2/middleware/cors"
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+// Métodos HTTP que acepta la configuración de cors
+var methods = strings.Join([]string{
+	fiber.MethodGet,
+	fiber.MethodPost,
+}, ",")
 
 // GetCorsConfig retornar la configuración de CORS
 func GetCorsConfig() cors.Config {
 	return cors.Config{
 		AllowOrigins:     "http://localhost:5173, http://localhost:3000, http://localhost:4321",
-		AllowMethods:     "GET, POST",
+		AllowMethods:     methods,
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept",
 	}
