44654277df4f1db111cd8e726c4d71a136968156
Mayer Chaves <andres.20.chaves@gmail.com>
2024-03-31 14:02:51 -0500

    agregando ruta de prueba

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/Mayer-04/fiber-authentication/handler"
+	"github.com/Mayer-04/fiber-authentication/middlewares"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
@@ -12,4 +13,7 @@ func SetupRoutes(app *fiber.App) {
 
 	auth.Post("/register", handler.Register)
 	auth.Post("/login", handler.Login)
+	auth.Get("/user", middlewares.VerifyToken, func(c *fiber.Ctx) error {
+		return c.SendString("Ruta privada")
+	})
 }
