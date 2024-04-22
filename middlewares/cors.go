83f61db3d2f512261ea8ae58ff46ef481dc63f71
Mayer-04 <andres.20.chaves@gmail.com>
2024-04-22 12:28:56 -0500

    mejorando código

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -7,18 +7,35 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-// Métodos HTTP que acepta la configuración de cors - GET Y POST
-var methods = strings.Join([]string{
+// corsOrigins es un slice que contiene los origenes aceptados por la configuración de CORS
+var corsOrigins = []string{
+	"http://localhost:5173",
+	"http://localhost:3000",
+	"http://localhost:4321",
+}
+
+// origins es una cadena que contiene la lista de origenes aceptados separados por comas
+var origins = strings.Join(corsOrigins, ",")
+
+// fiberMethods es un slice que contiene los métodos HTTP aceptados por la configuración de CORS
+var fiberMethods = []string{
 	fiber.MethodGet,
 	fiber.MethodPost,
-}, ",")
+}
 
-// GetCorsConfig retornar la configuración de CORS
+// methods es una cadena que contiene la lista de métodos HTTP aceptados separados por comas
+var methods = strings.Join(fiberMethods, ",")
+
+// GetCorsConfig retorna la configuración de CORS predeterminada para la aplicación
 func GetCorsConfig() cors.Config {
 	return cors.Config{
-		AllowOrigins:     "http://localhost:5173, http://localhost:3000, http://localhost:4321",
-		AllowMethods:     methods,
+		// AllowOrigins define los orígenes permitidos para las solicitudes CORS
+		AllowOrigins: origins,
+		// AllowMethods define los métodos HTTP permitidos en las solicitudes CORS
+		AllowMethods: methods,
+		// AllowCredentials indica si se permiten credenciales en las solicitudes CORS
 		AllowCredentials: true,
-		AllowHeaders:     "Origin, Content-Type, Accept",
+		// AllowHeaders define los encabezados HTTP permitidos en las solicitudes CORS
+		AllowHeaders: "Origin, Content-Type, Accept",
 	}
 }
